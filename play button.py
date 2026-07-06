e438c2c5306efb7315c0e16edc297bd694821a39
Lloyd Aboulnaja <lloydaboulnaja@gmail.com>
2026-07-06 21:36:51 +0100

    Add play image to play button

--- play button.py
+++ play button.py
@@ -1,2 +1,6 @@
+play_image = CTkImage(light_image=Image.open("PLAY.png"),
+                                  dark_image=Image.open("PLAY.png"),
+                                  size=(200, 200))
+
 play_button = CTkButton(master=window,text="",command=play,image=play_image,fg_color='transparent')
 play_button.place(relx=0.2,rely=0.3)

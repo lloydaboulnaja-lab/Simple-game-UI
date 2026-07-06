89ea5af8fb6ca1d3f07911134435044893b5c124
Lloyd Aboulnaja <lloydaboulnaja@gmail.com>
2026-07-06 21:39:03 +0100

    Add image to options button

--- options button.py
+++ options button.py
@@ -1,2 +1,7 @@
+set_image = CTkImage(light_image=Image.open("SETTINGS.png"),
+                                  dark_image=Image.open("SETTINGS.png"),
+                                  size=(200,200))
+
+
 options_button = CTkButton(master=window,text='',command=options,image=set_image,fg_color='transparent')
 options_button.place(relx=0.2,rely=0.5)

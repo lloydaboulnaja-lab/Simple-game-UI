ae071bdcd8d79c3fff43717d6c58bb834ed7a5dc
Lloyd Aboulnaja <lloydaboulnaja@gmail.com>
2026-07-06 21:42:03 +0100

    Update exit button.py

--- exit button.py
+++ exit button.py
@@ -1,2 +1,6 @@
+exit_image = CTkImage(light_image=Image.open("EXIT.png"),
+                                  dark_image=Image.open("EXIT.png"),
+                                  size=(200, 200))
+
 exit_button = CTkButton(master=window,text='',command=exiting, image=exit_image,fg_color="transparent")
 exit_button.place(relx=0.2,rely=0.7)
